fcf0f1e05bd225f3eb5895ec1ccf2714fa956a89
Yaswanth Rahul <yashwanthrahul5126@gmail.com>
2025-11-25 23:32:49 +0100

    chore: add app/head.tsx to set SVG favicon and avoid ICO decoding

---
+++ src/app/head.tsx
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="icon" href="/file.svg" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </>
+  )
+}
