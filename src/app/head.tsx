b07c71f861757c0cabea91a05f8a6288760413b7
Yaswanth Rahul <yashwanthrahul5126@gmail.com>
2025-11-25 23:46:59 +0100

    chore: use /favicon-32x32.png in app head

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <link rel="icon" href="/file.svg" />
+      <link rel="icon" href="/favicon-32x32.png" sizes="32x32" type="image/png" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </>
   )
